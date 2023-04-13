138623cde702ea02dcdac5ffa748a6cf08cc09b2
josefinabayugar <jbayugar@udesa.edu.ar>
2023-04-13 10:23:41 -0300

    arreglo rutas profile

--- routes/profile.js
+++ routes/profile.js
@@ -2,6 +2,7 @@ let express = require("express");
 let router = express.Router();
 let userController = require("../controllers/userController");
 
+router.get("/", userController.profile)   // 
 router.get("/edit", userController.profileEdit);
 router.get("/id/:id", userController.profile);
 
