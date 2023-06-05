3ccff2b2decafc488f0c70f2eeb9642a83b03ffe
sofialopez30 <slopezmoreno@udesa.edu.ar>
2023-06-05 10:56:35 -0300

    subo modelos de usuario

---
+++ database/models/usuario.js
@@ -0,0 +1,55 @@
+module.exports = (sequelize, dataTypes) => {
+
+    let alias = 'Usuario';
+
+    let columnas = {
+        id : {
+            type: dataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        email : {
+            type: dataTypes.STRING
+        }, 
+        contrasenia : {
+            type: dataTypes.STRING
+        }, 
+        fotoPerfil : {
+            type: dataTypes.STRING
+        }, 
+        fecha : {
+            type: dataTypes.DATE
+        }, 
+        numDocumento : {
+            type: dataTypes.INTEGER
+        }, 
+        createdAt : {
+            type: dataTypes.DATE
+        },
+        updatedAt : {
+            type: dataTypes.DATE
+        }
+    };
+
+    let configuraciones = {
+        tableName : 'infoUsuarios',
+        timestamps : true,
+        underscored : false
+    }
+
+    const Usuario = sequelize.define(alias, columnas, configuraciones);
+
+    Usuario.associate = (models) => {
+        Usuario.hasMany(models.Producto, {
+            as : 'usuario_producto',
+            foreignKey : 'infoUsuario_id'
+        })
+
+        Usuario.hasMany(models.Comentario, {
+            as : 'usuario_comentario',
+            foreignKey : 'infoUsuario_id'
+        })
+    }
+
+    return Usuario
+}
