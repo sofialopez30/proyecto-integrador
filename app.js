78fad9ec6293350e0b5a89e1427ab26bfea5103a
vreyleyes <vreyleyes@udesa.edu.ar>
2023-06-08 21:11:57 -0300

    app js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require("express-session");
 
 var indexRouter = require('./routes/index'); 
 var usersRouter = require('./routes/users'); 
@@ -19,6 +20,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+  secret: 'cerveza',
+  resave: false,
+  saveUninitialized: true
+}))
+
+app.use(function(req, res, next) {
+  if (req.session.user != undefined) {
+    res.locals.user = req.session.user;
+    return next();
+  };
+  return next();
+})
 
 app.use('/', indexRouter); 
 app.use('/users', usersRouter); 
